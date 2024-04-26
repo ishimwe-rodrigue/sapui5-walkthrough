86c1a5ddc310a3e3a167f358d471ec08ffb95a6d
Ishimwe Rodrigue <rodrigue.ishimwe@amalitech.com>
2024-04-26 15:09:25 +0200

    Step 10: Component Configuration

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,40 +1,28 @@
-sap.ui.define(
-  [
+sap.ui.define([
     "sap/ui/core/UIComponent",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel",
-  ],
-  (UIComponent, JSONModel, ResourceModel) => {
+    "sap/ui/model/json/JSONModel"
+ ], (UIComponent, JSONModel) => {
     "use strict";
-
-    return UIComponent.extend("ui5.walkthrough.Component", {
-      metadata: {
-        interfaces: ["sap.ui.core.IAsyncContentCreation"],
-        rootView: {
-          viewName: "sapui5.walkthrough.main.view.App",
-          type: "XML",
-          id: "app",
-        },
-      },
-
-      init() {
-        // call the init function of the parent
-        UIComponent.prototype.init.apply(this, arguments);
-        // set data model
-        const oData = {
-          recipient: {
-            name: "World",
-          },
-        };
-        const oModel = new JSONModel(oData);
-        this.setModel(oModel);
-
-        // set i18n model
-        const i18nModel = new ResourceModel({
-          bundleName: "sapui5.walkthrough.main.i18n.i18n",
-        });
-        this.setModel(i18nModel, "i18n");
-      },
+ 
+    return UIComponent.extend("sapui5.walkthrough.main.Component", {
+       metadata : {
+          interfaces: ["sap.ui.core.IAsyncContentCreation"],
+          manifest: "json"
+       },
+ 
+       init() {
+          // call the init function of the parent
+          UIComponent.prototype.init.apply(this, arguments);
+ 
+          // set data model
+          const oData = {
+             recipient : {
+                name : "SAPUI5"
+             }
+          };
+          const oModel = new JSONModel(oData);
+          this.setModel(oModel);
+       }
     });
-  }
-);
+ });
+ 
